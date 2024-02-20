310e1f64524e28d65512a026c0bfc71b7f7e16aa
David Hoyt <h02332@gmail.com>
2024-02-20 10:11:51 -0500

    Delete code/iOSOnMac/cx.srd.img-fuzz-001/cx.srd.img-fuzz-001/ViewController.h

--- code/iOSOnMac/cx.srd.img-fuzz-001/cx.srd.img-fuzz-001/ViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
-
-@property (weak, nonatomic) IBOutlet UIImageView *originalImageView;
-@property (weak, nonatomic) IBOutlet UIImageView *fuzzedImageView;
-
-@end
-
